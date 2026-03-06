ae02677ab863bb0a7815ccffa316f8212671c3c8
LAVÍNIA MARIA SILVA CAMPOS <lavinia.campos806@portalsesisp.org.br>
2026-03-06 13:22:20 -0300

    alunoB inserindo linha6 vazia e linha 7 com código

--- conflitos.ts
+++ conflitos.ts
@@ -2,3 +2,6 @@ console.log(`alunoA alterou a linha 1 do arquivo conflito.ts`);
 console.log(`alunoB inserindo a linha 2`);
 let concatenar: string = `Git` + `Hub`;
 console.log(`Concatenado ${concatenar}`);
+
+
+console.log(`Inserindo linha 7 pelo alunoB`);
